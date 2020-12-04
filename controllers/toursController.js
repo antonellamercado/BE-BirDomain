831f20d2972b201f7346069bb322bcc0d0ebfa5a
Antonella Mercado <antonellamercado@gmail.com>
2020-12-04 17:43:11 -0300

    trabajando en servicios de tour

---
+++ controllers/toursController.js
@@ -0,0 +1,70 @@
+const Tours = require('../models/Tours');
+const {check} = require ('express-validator');
+///////////////////////////////////////////////////////
+//crear tour
+exports.createTour = async(req,res) => {
+
+    //valida que el request no tenga errores
+    const errors = check(req)
+    if (!errors.isEmpty()){
+        return  res.status(400).json({errors: errors.array});
+    }
+    // hago destructuring de lo que trae el req
+    const {title, body, description, info, img, imgD, price, lat, latObs, dias, ecoregiones, especies, destacado } = req.body;
+
+    try
+    {
+    // crea nuevo tour en el  modelo Tours
+    let tour = new Tours(req.body);
+    // guarda nuevo tour a la BD
+    await tour.save();
+    }
+    catch(error){
+        console.log('error al crear usuario', error);
+        res.status(400).json({msg: 'Lo siento, ocurrio un error'});
+    }
+    res.status(200).json({msg: 'Tour creado correctamente'});
+    res.end();
+}
+////////////////////////////////////////////////////////
+//editar tour
+exports.update = async(req,res) => {
+//crea obj
+const tour = new Tours ({
+    title: req.body.title,
+    description: req.body.description,
+    info:req.body.info,
+    body: req.body.info,
+    img: req.body.img,
+    imgD:req.body.imgD,
+    price: req.body.price,
+    dias: req.body.dias,
+    ecoregiones:req.body.ecoregiones,
+    especies: req.body.especies,
+    isDestacado: req.body.isDestacado
+})
+//busca para modificar el tour
+Tours.findByIdAndUpdate({id: req.body.id},{
+    $set: {
+        title: tour.title, 
+        description: tour.description,
+        info: tour.info,
+        body: tour.body,
+        img: tour.img,
+        imgD: tour.imgD,
+        price: tour.price,
+        dias: tour.dias,
+        ecoregiones: tour.ecoregiones,
+        especies: tour.especies,
+        isDestacado: tour.isDestacado
+    }
+})
+.catch((error) => {
+    console.log('error al editar tour', error)
+    res.status(400).json({msg: 'Ocurrio un error al editar tour'});
+})
+
+console.log('modificacion exitosa');
+res.status(200).json({msg: 'Tour editado correctamente'});
+res.end();
+}
---
+++ models/Tours.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const toursSchema =mongoose.Schema({
+title: 
+{
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+},
+body:
+{
+    type:String,
+    required: true,
+    trim: true
+},
+description:
+{
+    type: String,
+    required: true,
+    trim: true
+},
+info:
+{
+    type: String,
+    required: true,
+    trim: true
+},
+img: {
+    type: String,
+    required: true,
+    trim: true
+},
+imgD:{
+    type: String,
+    required: true,
+    trim: true
+},
+price:{
+    type: Number,
+    required: true,
+    trim: true
+},
+lat:{
+    type: Array,
+    required: true,
+    trim: true
+},
+latObs:{
+    type: Array,
+    required: true,
+    trim: true
+},
+dias:{
+    type: Number,
+    required: true,
+    trim: true
+},
+ecoregiones:{
+    type: String,
+    required: true,
+    trim: true
+},
+especies:{
+    type: Number,
+    required: true,
+    trim: true
+},
+isDestacado:{
+    type:Boolean,
+    required: true,
+    trim:true
+},
+})
+module.exports = mongoose.model('Tours', toursSchema);
