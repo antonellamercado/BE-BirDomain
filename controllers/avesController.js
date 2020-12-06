cf65ea53f57d45285202fd7ea27cbdf590b3bd25
Antonella Mercado <antonellamercado@gmail.com>
2020-12-06 17:28:46 -0300

    service ave - tour - ok

--- controllers/avesController.js
+++ controllers/avesController.js
@@ -6,7 +6,7 @@ const Aves = require('../models/Aves');
 const traerAves = async (req, res, next) => {
     try {
         const aves = await Aves.find({});
-        res.status(200).json({msg: 'Aves obtenidos correctamente', aves});
+        res.status(200).json({msg: 'Aves obtenidas correctamente', aves});
     } catch (error) {
         console.log('error al traer todos las aves', error);
         res.status(500).send({msg:'Ocurrio un error al traer todos las aves'});
@@ -15,3 +15,18 @@ const traerAves = async (req, res, next) => {
     res.end();
 }
 exports.traerAves = traerAves
+
+
+// crear ave
+
+exports.createAve = async(req, res) => {
+    const ave = new Aves(req.body);
+    try {
+        await ave.save();
+        res.status(200).json({ message: 'Ave creado correctamente'});
+    } catch (error) {
+        console.log('error al crear ave', error);
+        res.status(500).send({ msg: 'Ocurrio un error al crear ave' });
+    }
+    res.end();
+}
--- routes/avesRouter.js
+++ routes/avesRouter.js
@@ -2,4 +2,5 @@ var express = require ('express');
 const avesRouter = express.Router();
 const avesController = require('../controllers/avesController');
 avesRouter.get('/', avesController.traerAves);
+avesRouter.post('/', avesController.createAve);
 module.exports = avesRouter;
