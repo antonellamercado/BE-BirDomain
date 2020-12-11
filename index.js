7abea6fda1a60829f552a6b3a6c0c904c5855919
Antonella Mercado <antonellamercado@gmail.com>
2020-12-11 20:50:33 -0300

    tratando de subir api a heroku

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log (`the server has started on port: ${PORT}`));
+//const host = process.env.HOST || '0.0.0.0'; 
+
 
 //mongoose nos conecta a mongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
