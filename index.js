bddf60468c3afdeb8768b3a17fa27f6e41899b45
Antonella Mercado <antonellamercado@gmail.com>
2020-12-11 23:06:08 -0300

    heroku todavia no funciona

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(cors());
 //Puertos 
 //heroku hay que buscar
 //heroku usa un puerto y asigna
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log (`the server has started on port: ${PORT}`));
-//const host = process.env.HOST || '0.0.0.0'; 
-
-
 //mongoose nos conecta a mongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     useNewUrlParser:true,
@@ -30,6 +24,14 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     if (err) throw err;
     console.log("MongoDB connection established")
 });
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log (`the server has started on port: ${PORT}`));
+//const host = process.env.HOST || '0.0.0.0'; 
+
+
 
 
 //set routes
